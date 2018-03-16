515a9c4bb9b89cf24ce2f679c86ef93fd18bc60e
Tautvilas Mecinskas <tautvilas@wix.com>
2018-03-16 15:19:20 +0200

    refactor and cleanup local store

--- src/store/local.js
+++ src/store/local.js
@@ -1,8 +1,11 @@
 function LinguLocalStore() {
-  this.changeHandlers = {};
+  const changeHandlers = {};
+  const storage = (typeof localStorage !== 'undefined') ? localStorage.getItem('appStorage') : undefined;
+  Lingu.firstRun = storage ? false : true;
+  const _space_ = Lingu.firstRun ? {} : JSON.parse(storage);
 
-  this.watchObject = function(obj, prop) {
-    const changeHandlers = this.changeHandlers;
+
+  const _watchObject = function(obj, prop) {
     return new Proxy(obj, {
       set(target, key, value) {
         target[key] = value;
@@ -21,51 +24,47 @@ function LinguLocalStore() {
     });
   };
 
-  const storage = (typeof localStorage !== 'undefined') ? localStorage.getItem('appStorage') : undefined;
-  Lingu.firstRun = storage ? false : true;
-  Lingu.space = Lingu.firstRun ? {} : JSON.parse(storage);
-
   this.processEvents = function(events) {
     events.forEach(event => {
       if (event.type === 'update') {
         const objects = Lingu.query.many(Lingu.context.type + ' ' + event.query);
         objects.forEach(object => {
-          const target = Lingu.space[object._type][object._id];
+          const target = _space_[object._type][object._id];
           target[event.property] = event.value;
-          Lingu.space[object._type][object._id] = Object.assign({}, target); // trigger change handlers
+          _space_[object._type][object._id] = Object.assign({}, target); // trigger change handlers
         });
       } else if (event.type === 'set') {
         event.items.forEach(d => {
-          const target = Lingu.space[d._type][d._id];
+          const target = _space_[d._type][d._id];
           target[event.property] = event.value;
-          Lingu.space[d._type][d._id] = Object.assign({}, target); // trigger change handlers
+          _space_[d._type][d._id] = Object.assign({}, target); // trigger change handlers
         });
       } else if (event.type === 'remove') {
         event.items.forEach(c => {
           const parent = c._parent;
           if (parent) {
-            const parentList = Lingu.space[parent.type][parent.id][c._type];
+            const parentList = _space_[parent.type][parent.id][c._type];
             const index = parentList.findIndex((v) => {return v.id === c._id;});
             if (index !== -1) {
               parentList.splice(index, 1);
             }
           }
-          delete Lingu.space[c._type][c._id];
+          delete _space_[c._type][c._id];
         });
       } else if (event.type === 'add') {
         const appendTo = Lingu.context;
         const id = event.item.id;
         const abs = event.item.type;
         if (appendTo) {
-          if (!Lingu.space[appendTo.type][appendTo.id][abs]) {
-            Lingu.space[appendTo.type][appendTo.id][abs] = [];
+          if (!_space_[appendTo.type][appendTo.id][abs]) {
+            _space_[appendTo.type][appendTo.id][abs] = [];
           }
-          Lingu.space[appendTo.type][appendTo.id][abs].push({id: id});
+          _space_[appendTo.type][appendTo.id][abs].push({id: id});
         }
-        Lingu.space[abs][id] = {_parent: appendTo, _id: id, _type: abs};
+        _space_[abs][id] = {_parent: appendTo, _id: id, _type: abs};
       } else if (event.type === 'init') {
         const entity = event.entity;
-        Lingu.space[entity.type][entity.id] = {
+        _space_[entity.type][entity.id] = {
           _type: entity.type,
           _id: entity.id
         };
@@ -74,31 +73,31 @@ function LinguLocalStore() {
   }
 
   this.initiateSpaceDef = function(type) {
-    if (!Lingu.space[type]) {
-      Lingu.space[type] = this.watchObject({}, type);
+    if (!_space_[type]) {
+      _space_[type] = _watchObject({}, type);
     } else {
-      Lingu.space[type] = this.watchObject(Lingu.space[type], type);
+      _space_[type] = _watchObject(_space_[type], type);
     }
-    this.changeHandlers[type] = [];
+    changeHandlers[type] = [];
   }
 
   this.getSpace = function() {
-    return JSON.parse(JSON.stringify(Lingu.space));
+    return JSON.parse(JSON.stringify(_space_));
   }
 
   this.persistSpace = function() {
-    localStorage.setItem('appStorage', JSON.stringify(Lingu.space));
+    localStorage.setItem('appStorage', JSON.stringify(_space_));
   }
 
   this.subscribe = function(target, callback) {
-    this.changeHandlers[target].push(callback);
+    changeHandlers[target].push(callback);
   }
 
   this.initDone = function() {
-    Object.keys(this.changeHandlers).forEach((handler) => {
+    Object.keys(changeHandlers).forEach((handler) => {
       //console.log(handler, space[handler], changeHandlers[handler]);
-      if (Lingu.space[handler] && Object.keys(Lingu.space[handler]).length) {
-        this.changeHandlers[handler].forEach(handler => handler());
+      if (_space_[handler] && Object.keys(_space_[handler]).length) {
+        changeHandlers[handler].forEach(handler => handler());
       }
     });
   }
