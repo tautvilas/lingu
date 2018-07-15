debb245761b3cfdd411f28a30b74e0df3284b237
Tautvilas Mecinskas <tautvilas@wix.com>
2018-07-15 15:32:01 +0300

    login sreen

---
+++ examples/ratings/screens/login.jsx
@@ -0,0 +1,13 @@
+function Login(props) { //eslint-disable-line no-unused-vars
+  return (
+    <section className="loginScreen" {...props.attributes}>
+      <h1>Welcome to awesome ratings community!</h1>
+      <div className="loginBox">
+        <div>Username:<input /></div>
+        <div>Password:<input /></div>
+        <div><button className="loginButton">Login</button></div>
+      </div>
+    </section>
+  );
+}
+
